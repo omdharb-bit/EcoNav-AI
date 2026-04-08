e9b418a321644df5bbc596c04cba2d936406b1a5
Om Dhar Badgaiyan <omdharb@gmail.com>
2026-04-08 19:38:29 +0530

    commit

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -4,7 +4,6 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 from apps.backend.api.aqi import router as aqi_router
